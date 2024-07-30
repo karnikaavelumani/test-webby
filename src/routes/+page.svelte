c5506fef98530f0d32585c2866effb4e61dc5706
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2024-07-30 12:23:43 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    min-height: 100vh;
+    font-family: Arial, Helvetica, sans-serif;
+}
+  
+img {
+    max-width: 100px;
+    height: auto;
+}
+</style>
+  
+<div class="container">
+    <h2>test webby.</h2>
+    <img src="/assets/pk.jpg" alt="Dog holding a self portrait" />
+</div>
+  
